2e5fa07bff18afa2b8b58f70eb92c0c69cf213e5
Maud Gautier <maud.j.gautier@gmail.com>
2023-11-19 15:50:49 -0700

    feat(project04): implement Fill in assembly machine language

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,111 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Pseudo code
+// -----------
+//    start_screen = SCREEN
+//    end_screen = SCREEN + 8192 (i.e. 512*256/16)
+//    max_i = 8192 (i.e. 512*256/16)
+// TEST_KBD
+//    initialize i (i=0)
+//    if (KBD != 0) then goto BLACKEN
+//    (else if (KBD == 0) then goto WHITEN)
+// BLACKEN_LOOP
+//    // PSEUDO
+//    // for (i=start_screen; i<end_screen; i++) {
+//    //     RAM[i] = -1
+//    // }
+//    if (i == max_i) then goto TEST_KBD
+//    RAM[i] = -1
+//    i++
+//    goto BLACKEN_LOOP
+
+// WHITEN_LOOP
+//    // PSEUDO
+//    // for (i=start_screen; i<end_screen; i++) {
+//    //     RAM[i] = 0
+//    // }
+//    if (i == max_i) then goto TEST_KBD
+//    RAM[i] = 0
+//    i++
+//    goto WHITEN_LOOP
+
+
+   //  start_screen = SCREEN
+   @SCREEN
+   D=A
+   @start_screen
+   M=D
+
+   //  end_screen = SCREEN + 8192 (i.e. 512*256/16)
+   // max_i = 8192 (i.e. 512*256/16)
+   @8192
+   D=A
+   @max_i
+   M=D
+
+(TEST_KBD)
+   // initialize i
+   @i
+   M=0
+
+   //  if (KBD != 0) then goto BLACKEN
+   @KBD
+   D=M
+   @BLACKEN_LOOP
+   D;JNE
+
+   //  (else if (KBD == 0) then goto WHITEN)
+   @KBD
+   D=M
+   @WHITEN_LOOP
+   D;JEQ
+
+(BLACKEN_LOOP)
+   // if (i == max_i) then goto TEST_KBD
+   @max_i
+   D=M
+   @i
+   D=D-M
+   @TEST_KBD
+   D;JEQ
+
+   // RAM[i] = -1
+   @start_screen
+   D=M
+   @i
+   A=D+M
+   M=-1
+
+   // i++
+   @i
+   M=M+1
+
+   // goto BLACKEN_LOOP
+   @BLACKEN_LOOP
+   0;JMP
+
+(WHITEN_LOOP)
+   // if (i == max_i) then goto TEST_KBD
+   @max_i
+   D=M
+   @i
+   D=D-M
+   @TEST_KBD
+   D;JEQ
+
+   // RAM[i] = 0
+   @start_screen
+   D=M
+   @i
+   A=D+M
+   M=0
+
+   // i++
+   @i
+   M=M+1
+
+   // goto WHITEN_LOOP
+   @WHITEN_LOOP
+   0;JMP
