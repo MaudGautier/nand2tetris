3759e73e53549c41a2b8b745fdb9371b5626ce40
Maud Gautier <maud.j.gautier@gmail.com>
2023-11-19 14:50:40 -0700

    feat(project04): implement Mult in assembly machine language

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,84 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// Pseudo code
+// -----------
+//   n = R0
+//   m = R1
+//   i = 0
+//   total = 0
+//   R2 = total (0)
+// LOOP
+//   if (m - i == 0) then goto STOP
+//   total = total + n
+//   i++
+//   goto LOOP
+// END
+//   R2 = total
+
+   // n = R0
+   @R0
+   D=M
+   @n
+   M=D
+
+   // m = R1
+   @R1
+   D=M
+   @m
+   M=D
+
+   // i = 0
+   @0
+   D=A
+   @i
+   M=D
+
+   // total = 0
+   @0
+   D=A
+   @total
+   M=D
+
+   // R2 = total (0)
+   @total
+   D=M
+   @R2
+   M=D
+
+(LOOP)
+   // if (m - i == 0) then goto STOP
+   @m
+   D=M
+   @i
+   D=D-M
+   @STOP
+   D;JEQ
+
+   // total = total + n
+   @n
+   D=M
+   @total
+   //D=D+M
+   //M=D
+   M=D+M
+
+   // i++
+   @i
+   M=M+1
+
+   // goto LOOP
+   @LOOP
+   0;JMP
+
+(STOP)
+   // R2 = total
+   @total
+   D=M
+   @R2
+   M=D
+
+(END)
+   @END
+   0;JMP
